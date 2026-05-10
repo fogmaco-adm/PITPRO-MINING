4a745eeed6e44f95f81fbb585853c7010e907e47
fogmaco-adm <fogmacojaya@gmail.com>
2026-05-10 08:02:40 +0700

    tambah fitur download di web ke hp

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'pitpro-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './logo.png',
+    'https://unpkg.com/html5-qrcode'
+];
+
+// Tahap 1: Install & Simpan ke Memori HP
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Membuka cache dan menyimpan aset...');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Tahap 2: Gunakan Memori HP jika Internet Mati
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Jika file ada di memori HP (cache), pakai itu!
+                if (response) {
+                    return response; 
+                }
+                // Jika tidak ada, coba ambil dari internet
+                return fetch(event.request); 
+            })
+    );
+});
+
+// Tahap 3: Hapus Memori Lama jika ada Update Aplikasi
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
